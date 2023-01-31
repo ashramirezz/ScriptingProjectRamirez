b70af1256687b245ba6b06500f52cbbda771e877
ashramirezz <ashleymr146@gmail.com>
2023-01-31 10:45:22 -0800

    Linear Interpolation
    Created a new script and explained what each line of code was and did.

---
+++ ScriptingProjectRamirez/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+  // Mathf.Lerp is finding a value that is some percentage between two given values. Lerp functions can be used for different
+  types of values.
+  float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+  // Vector3.Lerp is another Lerp function. This one works similar to Mathf.Lerp but it's values are different. 
+    Vector3 from = new Vector3 (1f,2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+  //Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This line of code makes it so that the intensity of light starts at 0 and set to 4 after this update. It will 
+        continue to rise to 6, 7, 7.5 etc... This happens over  the course of several frames. 
+`       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+
+        //This line of code is different from the one above because the one above depends on the frame rate while this is based on per seconds.
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+*/
+}
