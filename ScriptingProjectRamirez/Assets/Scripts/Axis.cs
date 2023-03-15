e00a37e00e6a5fc30ad367006238afa0b8d4c384
ashramirezz <ashleymr146@gmail.com>
2023-03-15 11:41:44 -0700

    GetComponent
    Created 3 new scripts and a new scene then attached the first two scripts to the first gamer object and the last one to the second., adding a box collider to the first game object too.

---
+++ ScriptingProjectRamirez/Assets/Scripts/Axis.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    public float range;
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        Debug.Log("Value Returned: " + h.ToString("F2"));
+    }
+}
